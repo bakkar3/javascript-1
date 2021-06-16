becf0240865b237d00be4d2f2a8b7926299a15b9
Ammar Bakkar <amarbakar33@gmail.com>
2021-06-16 10:36:04 +0200

    ok

---
+++ js/forArray.js
@@ -0,0 +1,21 @@
+const colors = ['red','blue','green'];    const users = [
+	{
+		id : 232,
+		login: 'jim',
+		firstName : 'Jim',
+		lastName : 'Jones'
+	    },
+	{
+		id : 325,
+		login: 'angie',
+		firstName : 'Angie',
+		lastName : 'Axelton'
+	    },
+	{
+		id : 634,
+		login: 'joan',
+		firstName : 'Joan',
+		lastName : 'Jambler'
+	    }
+	];
+	console.log(id);
